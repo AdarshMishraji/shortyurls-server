3aac3f599b39e545de2699e138b8677892850478
AdarshMishra <adarshmishra969@gmail.com>
2024-08-23 20:43:01 +0530

    temp

--- internal/app/main.go
+++ internal/app/main.go
@@ -5,15 +5,13 @@ import (
 	"shorty-urls-server/internal/database"
 	"shorty-urls-server/internal/internal/utils"
 	"shorty-urls-server/internal/routes"
-
-	"github.com/joho/godotenv"
 )
 
 func Start() {
-	err := godotenv.Load()
-	if err != nil {
-		panic(err)
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	panic(err)
+	// }
 
 	gob.Register(utils.LocationInfo{})
 	gob.Register(utils.DeviceInfo{})
