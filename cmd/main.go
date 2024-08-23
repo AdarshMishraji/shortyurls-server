acbc2cfbd687c14f7d8368865efdba18a03e5a29
AdarshMishra <adarshmishra969@gmail.com>
2024-08-23 20:42:33 +0530

    env removed

--- cmd/main.go
+++ cmd/main.go
@@ -2,15 +2,14 @@ package main
 
 import (
 	"shorty-urls-server/internal/app"
-
-	"github.com/joho/godotenv"
+	// "github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		panic(err)
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	panic(err)
+	// }
 
 	app.Start()
 }
